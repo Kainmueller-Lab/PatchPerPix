35e7f78c9bf581e43d6aaedfaa9fd642b4244bfa
Peter Hirsch <peter.hirsch@mdc-berlin.de>
2023-06-14 14:25:46 -0400

    cleanup, remove unused code

--- PatchPerPix/util/__init__.py
+++ PatchPerPix/util/__init__.py
@@ -1,4 +1,3 @@
-from .convert import zarr2hdf, hdf2zarr
 from .postprocess import remove_small_components, relabel, color, \
 	postprocess_fg, postprocess_instances, hex_to_rgb
 from .selectGPU import selectGPU
--- PatchPerPix/vote_instances/__init__.py
+++ PatchPerPix/vote_instances/__init__.py
@@ -1,5 +1,3 @@
 from .vote_instances import main
-from .vote_instances_blockwise import main, stitch_vote_instances
-from .stitch_patch_graph import main, get_offsets, get_offset_str, load_input, verify_shape, write_output, write_output, clean_mask 
-from .ref_vote_instances_blockwise import main
+from .stitch_patch_graph import main, get_offsets, get_offset_str, load_input, verify_shape, write_output, write_output, clean_mask
 from .graph_to_labeling import affGraphToInstances
